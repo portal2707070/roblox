6b00f41dee78e18ddb4d208d51603a20a2d29fcb
portal2707070 <150728739+portal2707070@users.noreply.github.com>
2024-06-29 00:06:33 +0200

    Update tddreal.lua

--- tddreal.lua
+++ tddreal.lua
@@ -1,7 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HerrSimoNHub/ToiletAutoFarm/main/Script.lua"))()
---[[
-	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
-]]
+
 local delay = 0.1
 function send(text)
    local A_delay = text
@@ -10,7 +7,8 @@ function send(text)
    event:FireServer(A_delay, A_2)
 
 end
-while true do
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HerrSimoNHub/ToiletAutoFarm/main/Script.lua"))()
+while wait(delay) do
    send("i are using autofarm")
    wait(delay)
    send("i eated pee pee")
