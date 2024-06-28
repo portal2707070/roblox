c12835ed74ef223e5caf5f83f8d5b339976b3cf5
portal2707070 <150728739+portal2707070@users.noreply.github.com>
2024-06-29 00:10:40 +0200

    Update tddreal.lua

--- tddreal.lua
+++ tddreal.lua
@@ -1,19 +1,1 @@
-local delay = 0.1
-function send(text)
-   local A_delay = text
-   local A_2 = "All"
-   local event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
-   event:FireServer(A_delay, A_2)
-end
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HerrSimoNHub/ToiletAutoFarm/main/Script.lua"))()
-
-send("i are using autofarm")
-wait(delay)
-send("i eated pee pee")
-wait(delay)
-send("i eated pee ")
-wait(delay)
-send("i are noob")
-wait(delay)
-send("loool")
-
